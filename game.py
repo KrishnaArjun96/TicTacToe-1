bcb55392eca8d21185498711e1aea471649377ea
Rahul Agasthya <Rahul.Agasthya@stonybrook.edu>
2017-05-01 16:00:09 -0400

    I have added a new method called as "isOver()". Please go through the code. If everything is alright, replace the original "isOver()" method with the new one.

--- game.py
+++ game.py
@@ -40,10 +40,10 @@ class Game(object):
 
 
 		if self.board[0] == 'X' and self.board[1] == 'X' and self.board[2] == 'X':
-			over = True;
+			over = True
 			winner = 'X'
 		elif board[3] == 'X' and board[4] == 'X' and board[5] == 'X':
-			over = True;
+			over = True
 			winner = 'X'
 		elif board[6] == 'X' and board[7] == 'X' and board[8] == 'X':
 			over = True
@@ -55,10 +55,10 @@ class Game(object):
 			over = True
 			winner = 'X'
 		elif board[0] == 'O' and board[1] == 'O' and board[2] == 'O':
-			over = True;
+			over = True
 			winner = 'O'
 		elif board[3] == 'O' and board[4] == 'O' and board[5] == 'O':
-			over = True;
+			over = True
 			winner = 'O'
 		elif board[6] == 'O' and board[7] == 'O' and board[8] == 'O':
 			over = True
@@ -88,7 +88,70 @@ class Game(object):
 		self.over = over
 		return over
 
+	def isOver(self):
 
+		#Checks for Three in a column.
+		i = 0
+		while(i<3):
+			if self.board[i] == 'O' and self.board[i+3] == '0' self.board[i+6] == '0':
+				over = True
+				winner = 'X'
+			elif self.board[i] == 'X' and self.board[i+3] == 'X' self.board[i+6] == 'X':
+				over = True
+				winner = 'O'
+			else:
+				over = False
+			i++;
+
+		#Checks for Three in a row.
+		i = 0
+		while(i<9):
+			if self.board[i] == 'O' and self.board[i+1] == '0' self.board[i+2] == '0':
+				over = True
+				winner = 'X'
+			elif self.board[i] == 'X' and self.board[i+1] == 'X' self.board[i+2] == 'X':
+				over = True
+				winner = 'O'
+			else:
+				over = False
+			i = i + 3
+
+		#Checks for main diagonal
+		if self.board[0] == 'O' and self.board[4] == '0' self.board[8] == '0':
+			over = True
+			winner = 'X'
+		elif self.board[0] == 'X' and self.board[4] == 'X' self.board[8] == 'X':
+			over = True
+			winner = 'O'
+		else:
+			over = False
+
+		#Checks for the other diagonal
+		if self.board[2] == 'O' and self.board[4] == '0' self.board[6] == '0':
+			over = True
+			winner = 'X'
+		elif self.board[2] == 'X' and self.board[4] == 'X' self.board[6] == 'X':
+			over = True
+			winner = 'O'
+		else:
+			over = False
+
+		if over and  winner == 'X':
+			if self.one.getPiece() == 'X':
+				winner = self.one
+				over = True
+			else:
+				winner = self.two
+				over = True
+		elif over and winner == 'O':
+			if self.one.getPiece() == 'O':
+				winner = self.one
+				over = True
+			else:
+				winner = self.two
+				over = True
+		self.over = over
+		return over
 
 
 
