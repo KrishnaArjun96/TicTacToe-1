016e11f9c377cf5dd927217b2cd953e7f69a7fe1
Rahul Agasthya <Rahul.Agasthya@stonybrook.edu>
2017-05-02 22:34:41 -0400

    Errors in the server.py file and other related files are corrected.

--- Server.py
+++ Server.py
@@ -1,112 +1,121 @@
-#import socket module
-from socket import *
-import time
 import player
 import tictactoe
 
+# import socket module
+from socket import *
+import time
+
+
+def respond_to_client(player, message):
+    connectionSocket.sendTo(message, player.get_address)
+
+
+def get_player(address):
+    for player in players:
+        if player.get_address == address:
+            return player
+    return None
+
+
+def print_help():
+    ret = 'Help Menu:\n' + 'HELP:\t\tPrints this menu.\n'
+    ret += 'LOGIN <urs_id>:\tConnect to the server using the unique user id.\n'
+    ret += 'PLACE <position>:\tIssues a new move and the piece is placed at the position in an array.\n'
+    ret += 'EXIT:\t\tExits and disconnects.'
+    return ret
+
+
 serverSocket = socket(AF_INET, SOCK_STREAM)
 
-#Prepare a sever socket
+# Prepare a sever socket
 serverPort = 8080
-serverSocket.bind(('',serverPort))
+serverSocket.bind(('', serverPort))
 serverSocket.listen(2)
 game = None
 
 players = []
 
 while True:
-	#Establish the connection
-	print('Welcome to TicTacToe')
-	connectionSocket, addr =  serverSocket.accept()
-	try:
-		message =  connectionSocket.recv(8080)
-		cmd = message.split(' ')
-		# Contents of cmd are based on the options: LOGIN, HELP, PLACE or EXIT.
+    # Establish the connection
+    print('Welcome to TicTacToe')
+    connectionSocket, addr = serverSocket.accept()
+    try:
+        message = connectionSocket.recv(8080)
+        cmd = message.split(' ')
+        # Contents of cmd are based on the options: LOGIN, HELP, PLACE or EXIT.
 
-		if(cmd[0] == "LOGIN"):
-			#cmd shall be as follows: LOGIN <usr_id> <usr_address>.
-			#The first user to login shall be placed first in the list and hence gets to go first with 'X'
-			user_id = cmd[1]
-			arrival_time = time.time()
-			address = cmd[2]
+        if cmd[0] == "LOGIN":
+            # cmd shall be as follows: LOGIN <usr_id> <usr_address>.
+            # The first user to login shall be placed first in the list and hence gets to go first with 'X'
+            user_id = cmd[1]
+            arrival_time = time.time()
+            address = cmd[2]
 
-			p = None
-			if(len(players) == 0):
-				p = Player(user_id, arrival_time, address, 'X')
-			else:
-				p = Player(user_id, arrival_time, address, 'O')
+            p = None
+            if len(players) == 0:
+                p = player.Player(user_id, arrival_time, address, 'X')
+            else:
+                p = player.Player(user_id, arrival_time, address, 'O')
 
-			players.append(p)
+            players.append(p)
 
-			#Game starts only if the number of players is 2. 
-			if(len(players) == 2):
-				game = TicTacToe(players[0], players[1])
-				respond_to_client(players[0], "Please make your move.")
-				respond_to_client(players[1], "Please wait for your turn.")
-		
-		elif(cmd[0] == "PLACE"):
-			#cmd shall be as follows: PLACE <location> <usr_address>.
-			if(cmd[2] != game.get_turn()):
-				# If it is not the correct user, send appropirate message.
-				# The variable turn stores the address of the user who is supposed to play.
-				connectionSocket.sendTo("Please wait for your turn.", cmd[2])
-			else:
-				# Allows the user to make the move. 
-				player = get_player(cmd[2])
-				if(game.move(cmd[1], player.get_char())):
-					# Checks if the Game is OVER. Prints necessary messages if true. 
-					game_state = game.is_game_over()
-					if(game_state):
-						connectionSocket.sendTo("You lose!", cmd[2])
-						opponent = game.get_opponent(cmd[2]);
-						respond_to_client(opponent, "You win!")
-					# Updates the game board to all users. 
-					connectionSocket.sendAll(game.get_board())
+            # Game starts only if the number of players is 2.
+            if len(players) == 2:
+                game = tictactoe.TicTacToe(players[0], players[1])
+                respond_to_client(players[0], "Please make your move.")
+                respond_to_client(players[1], "Please wait for your turn.")
 
-		elif(cmd[0] == "EXIT"):
-			#cmd shall be as follows: EXIT <usr_address>.
-			player = get_player(cmd[1])
+        elif cmd[0] == "PLACE":
+            # cmd shall be as follows: PLACE <location> <usr_address>.
+            if cmd[2] != game.get_turn():
+                # If it is not the correct user, send appropirate message.
+                # The variable turn stores the address of the user who is supposed to play.
+                connectionSocket.sendTo("Please wait for your turn.", cmd[2])
+            else:
+                # Allows the user to make the move.
+                player = get_player(cmd[2])
+                if game.move(cmd[1], player.get_char()):
+                    # Checks if the Game is OVER. Prints necessary messages if true.
+                    game_state = game.is_game_over(player.get_char())
+                    if game_state:
+                        connectionSocket.sendTo("You lose!", cmd[2])
+                        opponent = game.get_opponent(cmd[2])
+                        respond_to_client(opponent, "You win!")
+                # Updates the game board to all users.
+                connectionSocket.sendAll(game.get_board())
 
-			# Sets status of opponent to 'Available'
-			opponent = game.get_opponent(cmd[1])
-			opponent.set_status("Available")
+        elif cmd[0] == "EXIT":
+            # cmd shall be as follows: EXIT <usr_address>.
+            player = get_player(cmd[1])
 
-			respond_to_client(player, "Thank You for using this application.")
-			respond_to_client(opponent, "Your opponent exited the game.")
+            # Sets status of opponent to 'Available'
+            opponent = game.get_opponent(cmd[1])
+            opponent.set_status("Available")
 
-			# Removes from the list of players.
-			# Removes from the game
-			players.remove(player)
-			if(game.is_player_one(player)):
-				game.set_player_one(None)
-			if(game.is_player_two(player)):
-				game.set_player_two(None)
+            respond_to_client(player, "Thank You for using this application.")
+            respond_to_client(opponent, "Your opponent exited the game.")
 
-		elif(cmd[0] == "HELP"):
-			# cmd shall be as follows: HELP <usr_address>.
-			# Prints the help menu.
-			player = get_player(cmd[1])
-			respond_to_client(player, print_help())
+            # Removes from the list of players.
+            # Removes from the game
+            players.remove(player)
+            if game.is_player_one(player):
+                game.set_player_one(None)
+            if game.is_player_two(player):
+                game.set_player_two(None)
 
-		else:
-			#error message.
+        elif cmd[0] == "HELP":
+            # cmd shall be as follows: HELP <usr_address>.
+            # Prints the help menu.
+            player = get_player(cmd[1])
+            respond_to_client(player, print_help())
 
-	except IOError:
-		#Send response message for file not found
-		connectionSocket.send('HTTP/1.1 404 NOT FOUND!\r\n')	
-		#Close client socket
-		connectionSocket.close()
-serverSocket.close() 
+        # else:
+            # error message.
 
-def respond_to_client(player, message):
-	connectionSocket.sendTo(message, player.get_address)
+    except IOError:
+        # Send response message for file not found
+        connectionSocket.send('HTTP/1.1 404 NOT FOUND!\r\n')
+        # Close client socket
+        connectionSocket.close()
 
-def get_player(address):
-	for player in players:
-		if(player.get_address == address):
-			return player
-	return None
-
-def print_help():
-	ret = 'Help Menu:\n' + 'HELP:\t\tPrints this menu.\n' + 'LOGIN <urs_id>:\tConnect to the server using the unique user id.\n'
-	ret = ret + 'PLACE <position>:\tIssues a new move and the peice is placed at the position in an array.\n' + 'EXIT:\t\tExits and disconnects.'
+    serverSocket.close()
--- client.py
+++ client.py
@@ -1,52 +1,53 @@
 import socket
 import sys
 
+
 def help():
-	print("login <name> : logs you into the server with user id <name>")
-	print("place <n> : place piece in cell <n> if it is a valid move")
-	print("exit : exits you from the game")
+    print("login <name> : logs you into the server with user id <name>")
+    print("place <n> : place piece in cell <n> if it is a valid move")
+    print("exit : exits you from the game")
+
 
 def main():
-	# Create a TCP/IP socket
-	print(str(socket.gethostbyname(socket.gethostname())))
-	print(str(socket.gethostname()))
-	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Create a TCP/IP socket
+    print(str(socket.gethostbyname(socket.gethostname())))
+    print(str(socket.gethostname()))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-	port_number = input("Enter the port:")
-	machine = input("Machine name:")
+    port_number = input("Enter the port:")
+    machine = input("Machine name:")
 
-	# Connect the socket to the port where the server is listening
-	sock.connect((machine, port_number))
-	#address = sock.recv(1024)
+    # Connect the socket to the port where the server is listening
+    sock.connect((machine, port_number))
+    # address = sock.recv(1024)
 
-	#logs in user in the format LOGIN <userid>
-	command = input("Please log in:")
-	userid = command
-	command = "LOGIN " + userid + " "
-	sock.sendall(command)
+    # logs in user in the format LOGIN <userid>
+    command = input("Please log in:")
+    userid = command
+    command = "LOGIN " + userid + " "
+    sock.sendall(command)
 
-	#server sends client its address tuple to store for future responses
-	addr = sock.recv(1024)
-	
-	while True:
-		data = sock.recv(1024)
-		print(str(data))
+    # server sends client its address tuple to store for future responses
+    addr = sock.recv(1024)
 
-		#Still requires response interpretation 
-		#display updated board
-		#respond appropriately
-		command = input(">:")
+    while True:
+        data = sock.recv(1024)
+        print(str(data))
 
-		while command == "help":
-			help()
-			command = input(">:")
-		if command == "EXIT":
-			command = "EXIT " + userid
-		else:
-			split = command.split()
-			command = split[0] + " " + split[1] + " " + "name " + userid
-		print(command)
-		sock.sendall(command)
+        # Still requires response interpretation
+        # display updated board
+        # respond appropriately
+        command = input(">:")
 
+        while command == "help":
+            help()
+            command = input(">:")
+        if command == "EXIT":
+            command = "EXIT " + userid
+        else:
+            split = command.split()
+            command = split[0] + " " + split[1] + " " + "name " + userid
+        print(command)
+        sock.sendall(command)
 
 if __name__ == "__main__": main()
